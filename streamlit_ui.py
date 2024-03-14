a0eb9054ddcb48be6b0a5c5b5b17b1ad6fd5abb4
Muhammad Abdullah Asif <m.abdullahasif844@gmail.com>
2024-03-15 02:10:37 +0500

    updated

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -20,7 +20,7 @@ if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history = [])
 
 # Display Form Title
-st.title("Chat with Google Gemini-1.0-Pro!")
+st.title("Welcome to VoxVista")
 
 # Display chat messages from history above current input box
 for message in st.session_state.chat.history[1:]:
